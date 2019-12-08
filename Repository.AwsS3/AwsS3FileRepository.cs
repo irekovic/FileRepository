22b100e88074ed61bfa96d59a926d01ad0054263
Ivan Zivanovic <izivanovic@ipan.eu>
2019-12-08 22:12:55 +0100

    Initital implementation of AWS S3

--- Repository.AwsS3/AwsS3FileRepository.cs
+++ Repository.AwsS3/AwsS3FileRepository.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
-using Amazon.S3;
 using Amazon.S3.Model;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -26,34 +25,53 @@ namespace Repository.AwsS3
             _awsS3Settings = options.Value;
         }
 
-
-        public Task<FileRepositoryOperationResult<(Stream stream, string contentType)>> FetchFile(string filePath)
+        public async Task<FileRepositoryOperationResult<(Stream stream, string contentType)>> FetchFile(string filePath)
         {
-            throw new NotImplementedException();
+            _logger.LogInformation($"Fetching resource from path {filePath}");
+            filePath = NormalizeFilePath(filePath);
+            
+            var request = new GetObjectRequest()
+            {
+                BucketName = _awsS3Settings.BucketName,
+                Key = filePath
+            };
+
+            try
+            {
+                var client = _amazonS3ClientFactory.GetClient();
+                var response = await client.GetObjectAsync(request);
+                var contentType = response.Headers["Content-Type"];
+                return FileRepositoryOperationResult<(Stream stream, string contentType)>.Success((response.ResponseStream, contentType));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ex.GetType().Name}: {ex.Message}");
+                return FileRepositoryOperationResult<(Stream stream, string contentType)>.Error(ex.Message);
+            }
         }
 
         public async Task<FileRepositoryOperationResult<bool>> PersistFile(string filePath, Stream fileContent, dynamic metadata)
         {
+            _logger.LogInformation($"Persisting resource to path {filePath}");
+            filePath = NormalizeFilePath(filePath);
             try
             {
-                _logger.LogInformation($"About to upload resource to path {filePath}");
                 using var transferUtility = _amazonS3ClientFactory.GetTransferUtility();
                 await transferUtility.UploadAsync(fileContent, _awsS3Settings.BucketName,filePath);
-            }
-            catch (AmazonS3Exception ex)
-            {
-                _logger.LogError($"Error encountered on server. Message:'{ex.Message}' when writing an object");
+                return FileRepositoryOperationResult<bool>.Success(true);
             }
             catch (Exception ex)
             {
-                _logger.LogError($"Unknown encountered on server. Message:'{ex.Message}' when writing an object");
+                _logger.LogError($"{ex.GetType().Name}: {ex.Message}");
+                return FileRepositoryOperationResult<bool>.Error(ex.Message);
             }
-
-            return FileRepositoryOperationResult<bool>.Success(true);
         }
+
 
         public async Task<FileRepositoryOperationResult<bool>> RemoveFile(string filePath, dynamic metadata)
         {
+            _logger.LogInformation($"Removing resource from path {filePath}");
+            filePath = NormalizeFilePath(filePath);
             using var client = _amazonS3ClientFactory.GetClient();
             var request = new DeleteObjectRequest
             {
@@ -64,6 +82,12 @@ namespace Repository.AwsS3
             var response = await client.DeleteObjectAsync(request);
             var success = response.HttpStatusCode == System.Net.HttpStatusCode.OK;
             return FileRepositoryOperationResult<bool>.Success(success);
+        }
+        
+        private static string NormalizeFilePath(string filePath)
+        {
+            filePath = string.Join('/', filePath.Split('/', StringSplitOptions.RemoveEmptyEntries));
+            return filePath;
         }
     }
 }
